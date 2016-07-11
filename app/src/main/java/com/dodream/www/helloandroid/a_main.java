54db1d2c0313e20ab87ca62a8b98135f21ccbef1
YoungGyun Lee <yk1226ull@gmail.com>
2016-07-11 11:17:35 +0900

    Initial commit

---
+++ app/src/main/java/com/dodream/www/helloandroid/a_main.java
@@ -0,0 +1,14 @@
+package com.dodream.www.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class a_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }//onCreate
+
+}//class
