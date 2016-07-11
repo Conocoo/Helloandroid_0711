7f4d3e432628c6068abed8ae0044ae17305dddab
YoungGyun Lee <yk1226ull@gmail.com>
2016-07-11 14:18:18 +0900

    Toast & Dial

--- app/src/main/java/com/dodream/www/helloandroid/a_main.java
+++ app/src/main/java/com/dodream/www/helloandroid/a_main.java
@@ -1,11 +1,14 @@
 package com.dodream.www.helloandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class a_main extends AppCompatActivity {
 
@@ -20,8 +23,8 @@ public class a_main extends AppCompatActivity {
         edit_input = (EditText) findViewById(R.id.edit_input);
         tv_show = (TextView) findViewById(R.id.text_show);
 
-        Button btn_send = (Button) findViewById(R.id.btn_send);
-        btn_send.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.btn_send);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //String text = edit_input.getText().toString();
@@ -29,6 +32,32 @@ public class a_main extends AppCompatActivity {
                 tv_show.setText(edit_input.getText().toString());
             }
         });
-    }//onCreate
 
+        button = (Button) findViewById(R.id.btn_toast);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = getResources().getString(R.string.toast);
+                Toast.makeText(a_main.this, s, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(a_main.this,R.string.toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button = (Button) findViewById(R.id.btn_google);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(intent);
+            }
+        });
+        button = (Button) findViewById(R.id.btn_call);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01083667792"));
+                startActivity(intent);
+            }
+        });
+    }//onCreate
 }//class
